62e3bfd31125e8cb95085e74d85b457f3230ac73
Teodor Szente <teodor98sz@gmail.com>
2018-03-15 12:57:58 +0200

    add bot

---
+++ MyBot.py
@@ -0,0 +1,34 @@
+"""
+Welcome to your first Halite-II bot!
+
+This bot's name is Settler. It's purpose is simple (don't expect it to win complex games :) ):
+1. Initialize game
+2. If a ship is not docked and there are unowned planets
+2.a. Try to Dock in the planet if close enough
+2.b If not, go towards the planet
+
+Note: Please do not place print statements here as they are used to communicate with the Halite engine. If you need
+to log anything use the logging module.
+"""
+# Let's start by importing the Halite Starter Kit so we can interface with the Halite engine
+import hlt
+# Then let's import the logging module so we can print out information
+import logging
+
+# GAME START
+# Here we define the bot's name as Settler and initialize the game, including communication with the Halite engine.
+game = hlt.Game("Idle")
+# Then we print our start message to the logs
+logging.info("Starting my Idle bot!")
+
+while True:
+    # TURN START
+    # Update the map for the new turn and get the latest version
+    game_map = game.update_map()
+
+    # Here we define the set of commands to be sent to the Halite engine at the end of the turn
+    command_queue = []
+    # For every ship that I control
+    game.send_command_queue(command_queue)
+    # TURN END
+# GAME END
